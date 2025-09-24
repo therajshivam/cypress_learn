25d2a3493e9277fe89ab2c51d9d59e2717879138
Shivam Raj <rajshivam5984@gmail.com>
2025-09-24 16:24:35 +0530

    first_tutorial

---
+++ CypressAutomation/cypress/integration/examples/Test1.js
@@ -0,0 +1,21 @@
+/// <reference types="Cypress" />
+
+// cypress code
+// this test file is called spec 
+// in js terminology any test file is called as spec file
+
+// describe('My First Test', () => {
+//   it('Does not do much!', () => {
+//     expect(true).to.equal(true)
+//   })
+// })
+
+describe('First Test Suit', function () {
+    it('First Test Case', function(){
+        // test steps
+        // open a website
+        cy.visit("https://rahulshettyacademy.com/seleniumPractise/#/");
+        cy.get
+    })
+} )   
+
