35eb929bd58e78a3b1c83e9190284dedb4e1a5db
Shivam Raj <rajshivam5984@gmail.com>
2025-10-01 20:45:32 +0530

    test 1

--- CypressAutomation/cypress/integration/examples/Test1.js
+++ CypressAutomation/cypress/integration/examples/Test1.js
@@ -15,7 +15,22 @@ describe('First Test Suit', function () {
         // test steps
         // open a website
         cy.visit("https://rahulshettyacademy.com/seleniumPractise/#/");
-        cy.get('`z')
+        cy.get('.search-keyword').type('ca')
+        cy.wait(2000)
+        // selenium get hit url in browser, cypress get acts like findElement of selenium
+        cy.get('.product').should('have.length',5);
+        cy.get('.product:visible').should('have.length',4);
+        // parent child chaining
+        cy.get('.products').find('.product').should('have.length',4)
+        // cy.get('.products').find('.product').eq(2).contains('ADD TO CART').click()
+
+        cy.get('.products').find('.product').each(($el, index, $list) => {
+            const textVeg = $el.find('h4.product-name').text()
+            if (textVeg.includes('Cashews')) {
+                cy.wrap($el).find('button').click()
+            }
+        })
     })
+    // fixtures
 } )   
 
