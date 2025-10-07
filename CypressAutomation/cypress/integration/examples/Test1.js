b88d4b8acb74e9fdd03f26a2023fd5cf1dfad99f
Shivam Raj <rajshivam5984@gmail.com>
2025-10-07 13:31:55 +0530

    test

--- CypressAutomation/cypress/integration/examples/Test1.js
+++ CypressAutomation/cypress/integration/examples/Test1.js
@@ -24,6 +24,8 @@ describe('First Test Suit', function () {
         cy.get('.products').find('.product').should('have.length',4)
         // cy.get('.products').find('.product').eq(2).contains('ADD TO CART').click()
 
+        // Dynamically
+        // Iterate over an array of DOM elements
         cy.get('.products').find('.product').each(($el, index, $list) => {
             const textVeg = $el.find('h4.product-name').text()
             if (textVeg.includes('Cashews')) {
