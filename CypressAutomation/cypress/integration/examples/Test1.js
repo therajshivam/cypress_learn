49e0525588a63d6f99342eb58f9383081aedfd2b
Shivam Raj <rajshivam5984@gmail.com>
2025-10-08 12:40:57 +0530

    test

--- CypressAutomation/cypress/integration/examples/Test1.js
+++ CypressAutomation/cypress/integration/examples/Test1.js
@@ -46,6 +46,7 @@ describe('First Test Suit', function () {
         cy.get('.products').as('productLocator')
         cy.get('@productLocator').find('.product').eq(2).contains('ADD TO CART').click()
 
+        
         // diff bw log and console log
 
 
