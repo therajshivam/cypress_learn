5e9909742ed6990acf78556fd8d2a97b90eb5059
Shivam Raj <rajshivam5984@gmail.com>
2025-10-10 23:05:05 +0530

    test 4

---
+++ CypressAutomation/cypress/integration/examples/Test4.js
@@ -0,0 +1,13 @@
+/// <reference types="Cypress" />
+
+// Handling web control ui
+
+describe('Fourth Test Suit', function(){
+    it('Fourth Test Case', function(){
+
+        // 6. popups
+        // cypress auto handles the pop up, no need to write any code
+        // two types of pop ups : normal popups with ok button and confirm with ok or cancel popup
+
+    })
+})
