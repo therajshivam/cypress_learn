c7805eb33912a852e3f5124d6c9a75c9437bf143
Shivam Raj <rajshivam5984@gmail.com>
2025-10-17 13:24:27 +0530

    test

---
+++ CypressAutomation/cypress/integration/examples/Test6.js
@@ -0,0 +1,14 @@
+/// <reference types="Cypress" />
+
+// 8. Handling Web tables with Cypress using each command
+
+describe('Handling Web tables', function(){
+    it('Should Handle Web tables', function(){
+
+        cy.visit('https://rahulshettyacademy.com/AutomationPractice/')
+        
+        
+
+
+    })
+})
