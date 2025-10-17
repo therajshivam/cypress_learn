74dfb9de68d5330e6db2ec06a245075765215b3a
Shivam Raj <rajshivam5984@gmail.com>
2025-10-17 13:58:55 +0530

    test6

--- CypressAutomation/cypress/integration/examples/Test6.js
+++ CypressAutomation/cypress/integration/examples/Test6.js
@@ -6,10 +6,23 @@ describe('Handling Web tables', function(){
     it('Should Handle Web tables', function(){
 
         cy.visit('https://rahulshettyacademy.com/AutomationPractice/')
-        
-        
-        
 
+        cy.get('.table-display tr td:nth-child(2)').each(($e1, index, $list) => {
+
+            const text = $e1.text()
+            if (text.includes("Master Selenium Automation in simple Python Language")) {
+
+                // .next to go to immediate next sibling
+                // .next() should be used with .get() only
+                cy.get('.table-display tr td:nth-child(2)').eq(index).next().then(function(price){
+                    const priceText = price.text()
+                    expect(priceText).to.equal('25')
+                })
+
+            }
+
+        })
 
     })
+    
 })
