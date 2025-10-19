b52d0cca534845df5f1d939a7fc9fb33b60129e9
Shivam Raj <rajshivam5984@gmail.com>
2025-10-19 13:26:07 +0530

    test10 calenders

--- CypressAutomation/cypress/integration/examples/Test10.js
+++ CypressAutomation/cypress/integration/examples/Test10.js
@@ -4,9 +4,12 @@
 
 describe('Handling Calenders', function(){
     it('Should Handle Calenders', function(){
+
         const monthNumber = "6"
         const date = "15"
         const year = "2027"
+
+        const expectedList = [monthNumber, date, year]
 
         cy.visit('https://rahulshettyacademy.com/seleniumPractise/#/offers')
 
@@ -14,7 +17,16 @@ describe('Handling Calenders', function(){
         cy.get('.react-calendar__navigation__label__labelText--from').click()
         cy.get('.react-calendar__navigation__label__labelText--from').click()
 
-        cy.contains('button', year).click()
+        cy.contains('button', year).click() 
+
+        cy.get(".react-calendar__year-view__months__month").eq(Number(monthNumber) - 1).click()
+
+        cy.contains('abbr', date).click() 
+
+        // assertion
+        cy.get(".react-date-picker__inputGroup__input").each(($e1, index) => {
+            cy.wrap($e1).invoke('val').should('eq',expectedList[index])
+        })
 
     })
 })
