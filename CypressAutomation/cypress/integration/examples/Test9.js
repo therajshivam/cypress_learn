63b3f2a8e73d152839f243111e7ea2ce17154ac1
Shivam Raj <rajshivam5984@gmail.com>
2025-10-19 08:40:36 +0530

    Test9 iframe

---
+++ CypressAutomation/cypress/integration/examples/Test9.js
@@ -0,0 +1,27 @@
+/// <reference types="Cypress" />
+/// <reference types="cypress-iframe" />
+import 'cypress-iframe'
+
+
+// 8. Handling Frames
+
+describe('Handling Frames', function(){
+    it('Should Handle Frames', function(){
+
+        cy.visit('https://rahulshettyacademy.com/AutomationPractice/')
+        
+        // first install plugin using npm install -D cypress-iframe
+
+        cy.frameLoaded("#courses-iframe") // giving knowledge about iframe mode
+
+        // switch to iframe mode
+        cy.iframe().find('a[href*="mentorship"]').eq(0).click()
+        cy.wait(6000)
+
+        cy.iframe().find("h1[class*='pricing-title']").should('have.length', 2)
+
+
+
+    })
+})
+ 
