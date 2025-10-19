c2e461dd0693951280519e930246034b3418ecbe
Shivam Raj <rajshivam5984@gmail.com>
2025-10-19 07:39:36 +0530

    test8

--- CypressAutomation/cypress/integration/examples/Test5.js
+++ CypressAutomation/cypress/integration/examples/Test5.js
@@ -6,13 +6,16 @@ describe('Handling Child Windows', function(){
     it('Should Handle Child Window', function(){
 
         cy.visit('https://rahulshettyacademy.com/AutomationPractice/')
-        
+
+        // method 1. remove the target attribute
+        // method 2. target href attribute, grab it and go to using cy.url() 
+
         // remove the target= _blank attribute so it opens in the same tab
         // cypress can run in same tab, so take the content into the same tab
 
         cy.get('#opentab').invoke('removeAttr', 'target').click()
 
-        // cypress does not allow cross domain because of security issues and also due to how cypress is architecturally designed.
+        // cypress does not allow cross domain because of security issues and also due to how cypress is architecturally designed. Hence, we use cy.origin()
         cy.origin("https://www.qaclickacademy.com", () => 
         {
             cy.get(".navbar-nav > :nth-child(4) > a").click()
---
+++ CypressAutomation/cypress/integration/examples/Test8.js
@@ -0,0 +1,26 @@
+/// <reference types="Cypress" />
+
+// 8. Handling Child Windows
+
+describe('Handling Child Windows', function(){
+    it('Should Handle Child Windows', function(){
+
+        cy.visit('https://rahulshettyacademy.com/AutomationPractice/')
+
+        // like the child tabs, cypress cant handle child windows
+        // method 2 : target href attribute, grab it and go to url
+
+        // to get the attribute, we use jQuery method .prop()
+        // .prop() get the value of the property 
+
+        cy.get('#openwindow').then(function(el){
+            const url = el.prop('href')
+            cy.visit(url) // qaclickacademy.com
+
+            cy.origin(url, ()=>{
+                cy.get('div.sub-menu-bar a[href*="about"]').click()
+            })
+        })
+    })
+})
+ 
