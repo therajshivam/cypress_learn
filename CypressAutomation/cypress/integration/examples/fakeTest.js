6b0f7dcc9390f0fa895b402be3f747e24507bff8
Shivam Raj <rajshivam5984@gmail.com>
2025-11-25 22:52:21 +0530

    api

--- CypressAutomation/cypress/integration/examples/fakeTest.js
+++ CypressAutomation/cypress/integration/examples/fakeTest.js
@@ -1,5 +1,7 @@
 /// <reference types="Cypress" />
 
+// to mock response
+
 describe("Api Test", function () {
   it("Api Test", function () {
 
@@ -28,8 +30,16 @@ describe("Api Test", function () {
       }
     ).as('bookretrievals')
     cy.get('button[class="btn btn-primary"]').click()
-    cy.wait('@bookretrievals')
+    cy.wait('@bookretrievals').then(({request, response})=>{
+
+      cy.get('tr').should('have.length', response.body.length+1)
+      
+    })
     cy.get('p').should('have.text','Oops only 1 Book available')
-     
+    
+    // validation with
+    // length of response array = rows of table
+
+
   });
 });
---
+++ CypressAutomation/cypress/integration/examples/fakeTest2.js
@@ -0,0 +1,26 @@
+/// <reference types="Cypress" />
+
+// to mock request
+
+describe("Api Test", function () {
+  it("Api Test", function () {
+
+    cy.visit('https://rahulshettyacademy.com/angularAppdemo/');
+
+    // cy.intercept(method, URL, routeHandler?)
+    cy.intercept('GET', 'https://rahulshettyacademy.com/Library/GetBook.php?AuthorName=shetty', 
+        (req)=> 
+        {req.url="https://rahulshettyacademy.com/Library/GetBook.php?AuthorName=malhotra"
+        
+        req.continue((res)=>
+        {
+            expect(res.statusCode).to.equal(403)
+        })
+    }    
+        
+    )
+      
+
+
+  });
+});
