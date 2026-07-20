2e99cf4d8eba86e0b8754c212c3a20b5d095d3c6
Shivam Raj <rajshivam5984@gmail.com>
2026-07-20 22:28:11 +0530

    mod 6

--- CypressAutomation/cypress/integration/examples/Test1.js
+++ CypressAutomation/cypress/integration/examples/Test1.js
@@ -5,13 +5,24 @@
 // cypress spec
 describe("My first test suite", function () {
   it("My first testcase", function () {
+
     // write test steps here
     cy.visit("https://rahulshettyacademy.com/seleniumPractise/#/");
+
     cy.get('.search-keyword').type('ca')
+
     cy.wait(2000)
+
     // cy.get('.product').should('have.length', 4) -- failed because there were total 5 elements in which 1 was hidden.
+
     // Handling invisible elements.
     cy.get('.product:visible').should('have.length', 4)
+
+    // parent-child chaining
+    cy.get('.products').find('.product').should('have.length', 4)
+
+
+    cy.get('.products').find('.product')
   });
 });
 
