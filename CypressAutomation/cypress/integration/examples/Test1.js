a05a6455c6576627fe11a922db0f1accda04cf92
Shivam Raj <rajshivam5984@gmail.com>
2026-07-21 22:26:30 +0530

    mod 6

--- CypressAutomation/cypress/integration/examples/Test1.js
+++ CypressAutomation/cypress/integration/examples/Test1.js
@@ -22,8 +22,8 @@ describe("My first test suite", function () {
     cy.get('.products').find('.product').should('have.length', 4)
 
 
-    cy.get('.products').find('.product')
-    
+    cy.get('.products').find('.product').should('have.length', 4)
+
   });
 });
 
