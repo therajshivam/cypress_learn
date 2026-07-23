cefefe842fa01df532efaa12b4604c10bf8d2725
Shivam Raj <rajshivam5984@gmail.com>
2026-07-23 12:15:27 +0530

    test

--- CypressAutomation/cypress/integration/examples/Test1.js
+++ CypressAutomation/cypress/integration/examples/Test1.js
@@ -21,7 +21,8 @@ describe("My first test suite", function () {
     // parent-child chaining
     cy.get('.products').find('.product').should('have.length', 4)
 
-    cy.get('.products')
+      
+    
 
   });
 });
