058eb7787be871ec970005b889526a3b48f2e0eb
Shivam Raj <rajshivam5984@gmail.com>
2026-07-31 21:20:02 +0530

    framwork test : login page

---
+++ CypressAutomation/cypress/integration/examples/Test8Framework.js
@@ -0,0 +1,25 @@
+/// <reference types="Cypress" />
+
+describe('e2e ecommerce testing', () => {
+
+    it('submit order', () => {
+
+        // Login
+        cy.visit('https://rahulshettyacademy.com/loginpagePractise/');
+        cy.get('#username').type("rahulshettyacademy")
+        cy.get('#password').type('Learning@830$3mK2')
+        cy.contains('Sign In').click()
+
+        // Assertion of the next page
+        cy.contains('Shop Name').should('be.visible')
+
+        // validate/check 4 product carts on the page 
+        cy.get('app-card').should('have.length', 4)
+
+        // select product dynamically
+        const productName = 'Nokia Edge'
+        
+        
+
+    })
+})
